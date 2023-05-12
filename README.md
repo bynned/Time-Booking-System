84d4a91e8fee13499da0cbde8f2430b52caa3684
bynned <benny.cascarino@tuni.fi>
2023-05-12 22:58:04 +0300

    Added input fields, small css

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Time-Booking-System
 
 ## To run:
-
+> Move to the frontend, and the backend directory and run:
+```
+npm install
+```
 > in the root directory
 ```
 docker-compose up -d
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import React, { useState } from "react";
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Time Booking!</h1>
+      <br />
+      <label>
+        Name: <br />
+        <input type="text" name="name" />
+      </label>
+      <br />
+      <br />
+      <label>
+        Email: <br />
+        <input type="text" name="email" />
+      </label>
+      <br />
+      <br />
+      Date:
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+      />
     </div>
   );
 }
