222b6ff026d6a44661558f5d90dca9747a3eb267
bynned <benny.cascarino@tuni.fi>
2023-05-13 00:12:17 +0300

    Added and tested backend with .rest file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 4040;
+
+const bookings = [];
+
+app.post("/times", (req, res) => {
+  const { name, email, date } = req.body;
+  bookings.push({ name, email, date });
+  res.send("Booking added successfully");
+});
+
+app.get("/times", (req, res) => {
+  res.send(bookings);
+});
+
 app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
 });
-
-app.get("/", (req, res) => {
-    res.send("Heyyyy");
-});
-
