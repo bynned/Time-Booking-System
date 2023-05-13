9bde37828cadf049b0cdeb94bd6f0b11132c728f
bynned <benny.cascarino@tuni.fi>
2023-05-13 21:34:33 +0300

    Added fetch on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,28 +5,51 @@ import React, { useState } from "react";
 
 function App() {
   const [startDate, setStartDate] = useState(new Date());
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { name, email, date: startDate };
+    fetch("http://localhost:4040/times", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div className="App">
       <h1>Time Booking!</h1>
       <br />
-      <label>
-        Name: <br />
-        <input type="text" name="name" />
-      </label>
-      <br />
-      <br />
-      <label>
-        Email: <br />
-        <input type="text" name="email" />
-      </label>
-      <br />
-      <br />
-      Date:
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-      />
-      <button>Send!</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name: <br />
+          <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Email: <br />
+          <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        Date & Time:
+        <DatePicker selected={startDate} showTimeSelect onChange={(date) => setStartDate(date)} />
+        <br />
+        <br />
+        <button type="submit">Send!</button>
+      </form>
     </div>
   );
 }
